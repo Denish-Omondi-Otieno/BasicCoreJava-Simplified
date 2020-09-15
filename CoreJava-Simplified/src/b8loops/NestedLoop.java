d6e30c3ea05bd8814c342f0d3e3e66720767c34a
ADMIN <ADMIN@Academy.lan>
2020-09-15 12:24:31 -0500

    Nested Loop

---
+++ CoreJava-Simplified/src/b8loops/NestedLoop.java
@@ -0,0 +1,24 @@
+package b8loops;
+
+public class NestedLoop {
+	
+	// A nested loop is a loop within a loop
+	// An inner loop within the body of an outer one. 
+	
+	// In this program, the inner loop and the outer loop are initialized to 1
+	// The outer loop’s condition is  i <= 3 whereas  for inner loop is j <= 3
+	// Here outer loop iterates 3 times  
+	// For each iteration of the outer loop
+	// The inner loop executes three times.
+	
+	public static void main(String [] args) {
+		
+		for (int i = 1; i<=3; i++) {
+			for (int j = 1; j<=3; j++) {
+				System.out.println("value of i: " + i + "and value of j: " + j);
+			}
+		}
+		
+	}
+
+ }
